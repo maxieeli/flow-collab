e31187d6112526ef82033a52d7a5c64763caef78
maxiee <owenlideveloper@gmail.com>
2024-09-01 21:18:48 +0800

    feat: update collab core module

---
+++ collab/src/core/transaction.rs
@@ -0,0 +1,21 @@
+use yrs::updates::encoder::Encode;
+use yrs::{ReadTxn, StateVector, Transaction, TransactionMut};
+use crate::entity::EncodedCollab;
+
+pub trait DocTransactionExtension: ReadTxn {
+    fn get_encoded_collab_v1(&self) -> EncodedCollab {
+        EncodedCollab::new_v1(
+            self.state_vector().encode_v1(),
+            self.encode_state_as_update_v1(&StateVector::default()),
+        )
+    }
+    fn get_encoded_collab_v2(&self) -> EncodedCollab {
+        EncodedCollab::new_v2(
+            self.state_vector().encode_v2(),
+            self.encode_state_as_update_v2(&StateVector::default()),
+        )
+    }
+}
+
+impl<'a> DocTransactionExtension for Transaction<'a> {}
+impl<'a> DocTransactionExtension for TransactionMut<'a> {}
