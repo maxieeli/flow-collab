41f9c35f40580562582c7e787540e30e262b9d54
maxiee <owenlideveloper@gmail.com>
2024-09-08 18:35:17 +0800

    feat: update collab core module

---
+++ collab/src/core/mod.rs
@@ -0,0 +1,9 @@
+pub use yrs::sync::awareness;
+pub mod collab;
+pub mod collab_plugin;
+mod collab_search;
+pub mod collab_state;
+pub mod fill;
+pub mod origin;
+pub mod transaction;
+pub mod value;
